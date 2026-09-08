d7b1d49bcdf658cfa3591e6a7148cefa2e63109d
Apoorva-mishra01 <apoomishra008@gmail.com>
2026-09-08 09:20:03 +0530

    Time: 1 ms (83.65%), Space: 42.3 MB (85.26%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        String str= Integer.toString(n);
+        int len= str.length();
+        if(len<4){
+            return 0;
+        }
+        return n-999;
+    }
+}
