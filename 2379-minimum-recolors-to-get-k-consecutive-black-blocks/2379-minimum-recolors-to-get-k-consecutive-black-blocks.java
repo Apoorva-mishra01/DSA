d692925502b63ca016e4a33d75c270791bf3d48f
Apoorva-mishra01 <apoomishra008@gmail.com>
2026-09-22 15:20:50 +0530

    Time: 0 ms (100%), Space: 42.5 MB (96.12%) - LeetHub

---
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumRecolors(String blocks, int k) {
+      int n=blocks.length();
+      int white=0;
+      for(int i=0;i<k;i++){
+        if(blocks.charAt(i)=='W'){
+            white++;
+        }
+      }
+      int ans=white;
+      for(int i=k;i<n;i++){
+        if(blocks.charAt(i)=='W'){
+            white++;
+        }
+        if(blocks.charAt(i-k)=='W'){
+            white--;
+        }
+        ans=Math.min(ans,white);
+      }  
+      return ans;
+    }
+}
